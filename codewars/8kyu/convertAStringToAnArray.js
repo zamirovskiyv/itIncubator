f2e6d7b1f9b1e4f663cb3693006dac6baf59fad5
Vitaliy Zamirovskiy <vitaliy.zamirovskiy@nangu.tv>
2024-10-10 23:23:25 +0200

    Add task "Convert a string to an array"

---
+++ codewars/8kyu/convertAStringToAnArray.js
@@ -0,0 +1,16 @@
+// Write a function to split a string and convert it into an array of words.
+//
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+//
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+
+function stringToArray(string){
+    let arr = string.split(' ');
+    return arr
+}
+
+let str = "I love arrays they are my favorite";
+
+console.log(stringToArray(str))
