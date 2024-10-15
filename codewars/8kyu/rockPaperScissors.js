2a1e3c13b19c48d2cb36f431bb3fee5098a6c35e
zamirovskiyv <vitaliyzam@gmail.com>
2024-10-15 22:30:17 +0200

    Add task "Rock Paper Scissors"

---
+++ codewars/8kyu/rockPaperScissors.js
@@ -0,0 +1,27 @@
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+// Examples(Input1, Input2 --> Output):
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+
+const rps = (p1, p2) => {
+    if (p1 === p2) {
+        return "Draw!";
+    }
+
+    if (
+        (p1 === "scissors" && p2 === "paper") ||
+        (p1 === "rock" && p2 === "scissors") ||
+        (p1 === "paper" && p2 === "rock")
+    ) {
+        return "Player 1 won!";
+    } else {
+        return "Player 2 won!";
+    }
+};
+
+console.log(rps("scissors", "paper"));
+console.log(rps("scissors", "rock"));
+console.log(rps("paper", "paper"));
