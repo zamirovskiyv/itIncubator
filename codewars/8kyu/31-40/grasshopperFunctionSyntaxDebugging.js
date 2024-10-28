3608e1ad2e94dc83dd0f36df793bdb5a0e61a4de
zamirovskiyv <vitaliyzam@gmail.com>
2024-10-28 20:39:07 +0100

    Add task "Grasshopper - Function syntax debugging"

---
+++ codewars/8kyu/31-40/grasshopperFunctionSyntaxDebugging.js
@@ -0,0 +1,9 @@
+/*
+ Grasshopper - Function Syntax Debugging
+ A student was working on a function and made some syntax mistakes.
+ Help them find and fix the mistakes in the function.
+*/
+
+function main(verb, noun) {
+    return verb + noun
+}
