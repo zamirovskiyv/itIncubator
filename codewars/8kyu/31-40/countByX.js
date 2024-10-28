7988f698a380baf68186469efa17e9886ffdc256
zamirovskiyv <vitaliyzam@gmail.com>
2024-10-28 20:58:21 +0100

    Add task "Count by X"

---
+++ codewars/8kyu/31-40/countByX.js
@@ -0,0 +1,24 @@
+/*
+ Create a function with two arguments that will return an array of the first 'n' multiples of 'x'.
+
+ Assumptions:
+ - Both the given number (x) and the number of times to count (n) will be positive numbers greater than 0.
+
+ Return:
+ - The result should be returned as an array.
+
+ Examples:
+ x = 1, n = 10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ x = 2, n = 5  --> [2, 4, 6, 8, 10]
+*/
+
+function countBy(x, n) {
+    let z = [];
+    for (let i = 1; i <= n; i++) {
+        z.push(x * i);
+    }
+    return z;
+}
+
+console.log(countBy(1, 10));
+console.log(countBy(2, 5));
